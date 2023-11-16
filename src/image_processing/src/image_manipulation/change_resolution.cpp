0708a92e5629bcb5525eca41646e8a638ab9af06
ArmandoCivini <acivini@fi.uba.ar>
2023-11-16 00:01:38 -0300

    corrected resolution function

--- src/image_processing/src/image_manipulation/change_resolution.cpp
+++ src/image_processing/src/image_manipulation/change_resolution.cpp
@@ -1,17 +1,24 @@
 #include <string>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize2.h"
 
-void change_resolution(std::string path, int new_width, int new_height, std::string output_path) {
-    // Load the image
-    cimg_library::CImg<unsigned char> image(path.c_str());
+void change_resolution(std::string path, int output_width, int output_height, std::string output_path) {
+    int input_width, input_height, input_channels;
+    unsigned char* input_image = stbi_load(path.c_str(), &input_width, &input_height, &input_channels, 0);
 
-    // Display the original image
-    image.display("Original Image");
+    int output_channels = input_channels;
 
-    // Resize the image
-    cimg_library::CImg<unsigned char> resized_image = image.resize(new_width, new_height);
+    unsigned char* output_image = new unsigned char[output_width * output_height * output_channels];
 
-    // Display the resized image
-    resized_image.display("Resized Image");
+    stbir_resize_uint8_linear(input_image, input_width, input_height, 0,
+                       output_image, output_width, output_height, 0, static_cast<stbir_pixel_layout>(output_channels));
 
-    resized_image.save(output_path.c_str());
+    stbi_write_png(output_path.c_str(), output_width, output_height, output_channels, output_image, output_width * output_channels);
+
+    stbi_image_free(input_image);
+    delete[] output_image;
 }
