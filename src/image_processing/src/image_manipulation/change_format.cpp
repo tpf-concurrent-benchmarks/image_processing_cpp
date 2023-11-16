e8fd639397ba248d0ee8538f257766134ee360e7
ArmandoCivini <acivini@fi.uba.ar>
2023-11-16 17:50:37 -0300

    added function to convert from jpeg to png

---
+++ src/image_processing/src/image_manipulation/change_format.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+void change_format(std::string path, std::string output_path) {
+    int input_width, input_height, input_channels;
+    unsigned char* input_image = stbi_load(path.c_str(), &input_width, &input_height, &input_channels, 0);
+
+    stbi_write_png_compression_level = 9;
+    stbi_write_png(output_path.c_str(), input_width, input_height, input_channels, input_image, input_width * input_channels);
+}
--- src/image_processing/src/main.cpp
+++ src/image_processing/src/main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <string>
-#include "image_manipulation/change_resolution.cpp"
-// #define STB_IMAGE_IMPLEMENTATION
-// #include "image_manipulation/stb_image.h"
-// #define STB_IMAGE_WRITE_IMPLEMENTATION
-// #include "image_manipulation/stb_image_write.h"
+// #include "image_manipulation/change_resolution.cpp"
+#include "image_manipulation/change_format.cpp"
+
 
 int main() {
     std::string path = "/home/civa/image_processing_cpp/src/image_processing/resources/rust.jpeg";
-    // int width, height, channels;
-    // unsigned char* image = stbi_load(path.c_str(), &width, &height, &channels, 0);
+
     int new_width = 100;
     int new_height = 100;
+
     std::string output_path = "/home/civa/image_processing_cpp/src/image_processing/resources/rust.png";
-    change_resolution(path, new_width, new_height, output_path);
-    // stbi_write_png(output_path.c_str(), width, height, channels, image, width * channels);
+
+    // change_resolution(path, new_width, new_height, output_path);
+
+    change_format(path, output_path);
     return 0;
 }
