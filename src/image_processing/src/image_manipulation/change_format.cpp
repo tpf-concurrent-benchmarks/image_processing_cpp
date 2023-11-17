9a9474f1dc8dd7188cc1cce1ceacc97ba54df94e
ArmandoCivini <acivini@fi.uba.ar>
2023-11-17 18:06:35 -0300

    format code

--- src/image_processing/src/image_manipulation/change_format.cpp
+++ src/image_processing/src/image_manipulation/change_format.cpp
@@ -4,12 +4,14 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-void change_format(std::string path, std::string output_path) {
+void change_format(std::string path, std::string output_path)
+{
     int input_width, input_height, input_channels;
-    unsigned char* input_image = stbi_load(path.c_str(), &input_width, &input_height, &input_channels, 0);
+    unsigned char *input_image = stbi_load(path.c_str(), &input_width, &input_height, &input_channels, 0);
 
     stbi_write_png_compression_level = 9;
-    stbi_write_png(output_path.c_str(), input_width, input_height, input_channels, input_image, input_width * input_channels);
+    stbi_write_png(output_path.c_str(), input_width, input_height, input_channels, input_image,
+                   input_width * input_channels);
 
     stbi_image_free(input_image);
 }
--- src/image_processing/src/image_manipulation/crop.cpp
+++ src/image_processing/src/image_manipulation/crop.cpp
@@ -4,17 +4,22 @@
 #include "stb_image_write.h"
 #include <string>
 
-void _crop(unsigned char* input_image, std::string output_path, int x, int y, int width, int height, int input_width, int input_height, int input_channels) {
+void _crop(unsigned char *input_image, std::string output_path, int x, int y, int width, int height, int input_width,
+           int input_height, int input_channels)
+{
     x = std::max(0, std::min(x, input_width - 1));
     y = std::max(0, std::min(y, input_height - 1));
     width = std::min(width, input_width - x);
     height = std::min(height, input_height - y);
 
-    unsigned char* cropped_image = new unsigned char[width * height * input_channels];
+    unsigned char *cropped_image = new unsigned char[width * height * input_channels];
 
-    for (int j = 0; j < height; ++j) {
-        for (int i = 0; i < width; ++i) {
-            for (int c = 0; c < input_channels; ++c) {
+    for (int j = 0; j < height; ++j)
+    {
+        for (int i = 0; i < width; ++i)
+        {
+            for (int c = 0; c < input_channels; ++c)
+            {
                 cropped_image[(j * width + i) * input_channels + c] =
                     input_image[((j + y) * input_width + (i + x)) * input_channels + c];
             }
@@ -27,16 +32,18 @@ void _crop(unsigned char* input_image, std::string output_path, int x, int y, in
     delete[] cropped_image;
 }
 
-void crop(std::string path, std::string output_path, int x, int y, int width, int height) {
+void crop(std::string path, std::string output_path, int x, int y, int width, int height)
+{
     int input_width, input_height, input_channels;
-    unsigned char* input_image = stbi_load(path.c_str(), &input_width, &input_height, &input_channels, 0);
+    unsigned char *input_image = stbi_load(path.c_str(), &input_width, &input_height, &input_channels, 0);
 
     _crop(input_image, output_path, x, y, width, height, input_width, input_height, input_channels);
 }
 
-void crop_centered(std::string path, std::string output_path, int width, int height) {
+void crop_centered(std::string path, std::string output_path, int width, int height)
+{
     int input_width, input_height, input_channels;
-    unsigned char* input_image = stbi_load(path.c_str(), &input_width, &input_height, &input_channels, 0);
+    unsigned char *input_image = stbi_load(path.c_str(), &input_width, &input_height, &input_channels, 0);
 
     int x = (input_width - width) / 2;
     int y = (input_height - height) / 2;
